0a4843821e26a10e8b2eda0f7ee3dca3c8fff570
Ernest Lange <rnstlange@gmail.com>
2020-05-26 17:48:00 +0300

    Add cssnano

--- postcss.config.js
+++ postcss.config.js
@@ -4,8 +4,18 @@ const purgecss = require('@fullhuman/postcss-purgecss')({
 	defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
 })
 
+const cssnano = require('cssnano')({
+	preset: 'default'
+})
+
 const production = !process.env.ROLLUP_WATCH
 
 module.exports = {
-	plugins: [require('tailwindcss'), require('precss'), require('postcss-import'), require('autoprefixer'), ...(production ? [purgecss] : [])]
+	plugins: [
+		require('tailwindcss'),
+		require('precss'),
+		require('postcss-import'),
+		require('autoprefixer'),
+		...(production ? [purgecss, cssnano] : [])
+	]
 }
